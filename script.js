12afdfb4b2f2c05ef09f27985dc23c729c7feb99
Ayush Singh <99062720+ayush-singh05@users.noreply.github.com>
2023-09-16 20:05:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
-
+	
+	const form=document.getElementById("form1");
+let fullName=form["fname"].value + " "+ form["lname"].value
+	
+alert(fullName);
 }
+
